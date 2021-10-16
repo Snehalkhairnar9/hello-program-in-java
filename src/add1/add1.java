f24ba7f8efaf19db2890a935ccb26c3b1be982e4
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-10-16 11:00:56 +0100

    java program

---
+++ src/add1/add1.java
@@ -0,0 +1,9 @@
+package add1;
+
+public class add1 {
+
+	 public static void main(String[] args) 
+	 {
+		    System.out.println("add1");
+	 }
+}
---
+++ src/add1/string.java
@@ -0,0 +1,10 @@
+package add1;
+
+public class string {
+	 public static void main(String[] args) 
+	 {
+		    System.out.println("add1");
+	 }
+}
+
+
